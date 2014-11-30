f83b5dc78db870a275189ed4703dc8216a083816
apiepho <alpiepho@gmail.com>
2014-11-30 08:35:17 -0600

    adding workaround for menu action.  instead of using FirstResponder (as recommended) we are manually parsing the application menu and changing the submenu action.

--- HelloGoodbyeLabel/ViewController.m
+++ HelloGoodbyeLabel/ViewController.m
@@ -8,18 +8,54 @@
 
 #import "ViewController.h"
 
+@interface ViewController()
+@property (weak) IBOutlet NSTextField *label;
+@end
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
     // Do any additional setup after loading the view.
+    
+    // WORKAROUND: wanted to use First Responder for Menu Items, but could never get it working.  Instead,
+    // we will manually find our menu items and change the action selector.
+    
+    NSMenu *mainMenu = [[NSApplication sharedApplication] mainMenu];
+    NSInteger ourIndex = 0;
+    for (NSMenuItem *item in [mainMenu itemArray]) {
+        NSLog(@"%@", [item title]);
+        if ([@"Ours" isEqualToString:[item title]]) {
+            NSMenu *appMenu = [[mainMenu itemAtIndex:ourIndex] submenu];
+            for (NSMenuItem *item in [appMenu itemArray]) {
+                NSLog(@"%@", [item title]);
+                if ([@"Say Hello" isEqualToString:[item title]]) {
+                    [item setAction:@selector(sayHello:)];
+                }
+                if ([@"Say Goodbye" isEqualToString:[item title]]) {
+                    [item setAction:@selector(sayGoodbye:)];
+                }
+            }
+        }
+        ourIndex++;
+    }
 }
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
 
     // Update the view, if already loaded.
+}
+
+- (IBAction)sayHello:(id)sender {
+    NSLog(@"Hello");
+    [self.label setStringValue:@"Hello"];
+}
+
+- (IBAction)sayGoodbye:(id)sender {
+    NSLog(@"Goodbye");
+    [self.label setStringValue:@"Goodbye"];
 }
 
 @end
