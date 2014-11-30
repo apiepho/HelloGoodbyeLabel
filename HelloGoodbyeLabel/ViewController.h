5adbbe9393e958ad1aa258bd03b5161ec63ab156
apiepho <alpiepho@gmail.com>
2014-11-30 08:02:18 -0600

    Initial Commit

---
+++ HelloGoodbyeLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloGoodbyeLabel
+//
+//  Created by Al on 11/30/14.
+//  Copyright (c) 2014 Thatname Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ HelloGoodbyeLabelTests/HelloGoodbyeLabelTests.m
@@ -0,0 +1,40 @@
+//
+//  HelloGoodbyeLabelTests.m
+//  HelloGoodbyeLabelTests
+//
+//  Created by Al on 11/30/14.
+//  Copyright (c) 2014 Thatname Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface HelloGoodbyeLabelTests : XCTestCase
+
+@end
+
+@implementation HelloGoodbyeLabelTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
